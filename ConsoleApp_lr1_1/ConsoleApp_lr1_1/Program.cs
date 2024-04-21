7b64c42c03cb82720f16e3d7cf5cd8180a8c4bd8
Дарина Смолянчук (ТР-13) <119738358+DarinaSmolianchuk@users.noreply.github.com>
2024-04-21 19:43:22 +0300

    Add files via upload

---
+++ ConsoleApp_lr1_1/ConsoleApp_lr1_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Клас для виведення привітання
+/// </summary>
+public class Hello
+{
+    /// <summary>
+    /// Виводить привітання на консоль
+    /// </summary>
+    public void DisplayGreeting()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Створюємо екземпляр класу Hello
+        Hello myGreetings = new Hello();
+
+        // Викликаємо метод DisplayGreeting для виведення привітання
+        myGreetings.DisplayGreeting();
+    }
+}
