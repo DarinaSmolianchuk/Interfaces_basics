8131c045ab0ab797665df6d236277bdef5a3b191
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-04-24 01:11:37 +0300

    Commands and elements Binding

--- Lr2/Lr2/MainWindow.xaml.cs
+++ Lr2/Lr2/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lr2
 {
@@ -23,6 +13,54 @@ namespace Lr2
         public MainWindow()
         {
             InitializeComponent();
+
+            //Створення прив'язки та приєднання обробників
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, execute_Save, canExecute_Save);
+            //Реєстрація прив'язки
+            CommandBindings.Add(saveCommand);
+
+            //Відкрити
+            CommandBinding openCommand = new CommandBinding(ApplicationCommands.Open, execute_Open, canExecute_Open);
+            CommandBindings.Add(openCommand);
+
+            //Стерти
+            CommandBinding deleteCommand = new CommandBinding(ApplicationCommands.Delete, execute_Delete, canExecute_Delete);
+            CommandBindings.Add(deleteCommand);
+        }
+
+        void canExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        void execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("D:\\Мои документы\\0КПІ\\3 курс\\6 семестр\\Декларативне прог граф інт\\2\\myFile.txt", inputTextBox.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
+        //Відкрити
+        private void canExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            string defaultFolderPath = @"D:\Мои документы\0КПІ\3 курс\6 семестр\Декларативне прог граф інт\2";
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Text files (*.txt)|*.txt";
+            open.InitialDirectory = defaultFolderPath;
+            if (open.ShowDialog() == true)
+                inputTextBox.Text = File.ReadAllText(open.FileName);
+        }
+
+        //Стерти
+        private void canExecute_Delete(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        private void execute_Delete(object sender, ExecutedRoutedEventArgs e)
+        {
+            inputTextBox.Text = "";
         }
     }
 }
