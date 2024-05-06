2fc71d304a82cd4cabd29bdd3a98d253f31acdd3
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-05-06 10:34:59 +0300

    SQL DB Employees - create table

---
+++ Lr4/Lr4/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ПІБ] NVARCHAR(100) NOT NULL, 
+    [Підрозділ] INT NOT NULL, 
+    [Посада] NVARCHAR(30) NOT NULL, 
+    [Телефон] VARCHAR(16) NOT NULL
+)
