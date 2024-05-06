2fa57a77be6b3154c092e024c89a14d2be691568
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-05-06 23:37:27 +0300

    2

--- Lab4/Lab4/MainWindow.xaml.cs
+++ Lab4/Lab4/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,9 @@ namespace Lab4
         public MainWindow()
         {
             InitializeComponent();
+
+            var ShowInf = DataBase.loadDataTable();
+            
         }
     }
 }
