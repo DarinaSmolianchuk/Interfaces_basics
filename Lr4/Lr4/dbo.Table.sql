61bd173ddfd874eb1355a4369cea7953204665f8
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-05-06 10:54:41 +0300

    created and filled table Employees

--- Lr4/Lr4/dbo.Table.sql
+++ Lr4/Lr4/dbo.Table.sql
@@ -3,6 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [ПІБ] NVARCHAR(100) NOT NULL, 
     [Підрозділ] INT NOT NULL, 
-    [Посада] NVARCHAR(30) NOT NULL, 
+    [Посада] NVARCHAR(100) NOT NULL, 
     [Телефон] VARCHAR(16) NOT NULL
 )
