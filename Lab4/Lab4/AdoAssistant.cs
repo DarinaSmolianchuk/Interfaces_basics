7b2a65bb5818ffdffda936fc25edf59bb797a005
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-05-07 17:41:34 +0300

    ADO.net + Program interface and data binding

---
+++ Lab4/Lab4/AdoAssistant.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Lab4
+{
+    public class AdoAssistant
+    {
+        // Отримуємо рядок з'єднання з файлу App.config
+        String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionString_ADO"].ConnectionString;
+                
+        // Метод читання даних з DataTable
+        DataTable dt = null; // Посилання на об'єкт DataTable
+        public DataTable TableLoad()
+        {
+            if (dt != null) return dt; // Завантажимо таблицю лише один раз
+            // Заповнюємо об'єкт таблиці даними з БД
+            dt = new DataTable();
+            // Створюємо об'єкт підключення
+            using (SqlConnection сonnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = сonnection.CreateCommand(); // Створюємо об'єкт команди
+                SqlDataAdapter adapter = new SqlDataAdapter(command); // Створюємо об'єкт читання
+                //Завантажує дані 
+                command.CommandText = "Select Id," + "FullName," +
+                "Unit, Position, PhoneNumber From Employees";
+                try
+                {
+                    // Метод сам відкриває БД і сам її закриває
+                    adapter.Fill(dt);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Помилка підключення до БД:\n" + e);
+                 
+                }
+            }
+            return dt;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public static DataTable loadDataTable()
+//{
+//    //Виймаємо у полі рядок з'єднання з файлу App.config
+//    string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionString_ADO"].ConnectionString;
+//    SqlConnection connection = new SqlConnection(connectionString);
+//    connection.Open();
+//    //Запит
+//    string strSQL = "select * from Employees;";
+
+//    //Заповнення DataTable
+//    DataTable data = new DataTable();
+
+//    // Підготовка об'єкта команди
+//    SqlCommand command = new SqlCommand(strSQL, connection);
+//    SqlDataReader dataReader = command.ExecuteReader();
+
+//    // Заповнення DataTable даними з об'єкта читання
+//    data.Load(dataReader);
+//    dataReader.Close();
+//    connection.Close();
+
+//    return data;
+//}
+
--- Lab4/Lab4/MainWindow.xaml.cs
+++ Lab4/Lab4/MainWindow.xaml.cs
@@ -1,33 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Lab4
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            var ShowInf = DataBase.loadDataTable();
-            
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AdoAssistant myTable = new AdoAssistant();
+            list.SelectedIndex = 0;
+            list.Focus();
+            list.DataContext = myTable.TableLoad();
         }
     }
 }
+        //    var ShowInf = DataBase.loadDataTable();
+            
+
+
+        //}
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    list.SelectedIndex = 0;
+        //    list.Focus();
+        //    list.DataContext = DataBase.loadDataTable();
+        //}
