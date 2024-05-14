3f8612be10f6768360f9ef5fc4255d10b1dc716a
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-05-14 17:23:53 +0300

    Created queries to the Entity Framework

--- Lab5/Lab5/MainWindow.xaml.cs
+++ Lab5/Lab5/MainWindow.xaml.cs
@@ -1,28 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab5
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        EmployeesAndUnitsEntities context;
+        List<Employees> employees;
+        List<Units> units;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            context = new EmployeesAndUnitsEntities();
+            employees = new List<Employees>();
+            units = new List<Units>();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var emp in context.Employees)
+                employees.Add(emp);
+            EmployeesDir.ItemsSource = employees;
+
+            foreach (var emp in context.Units)
+                units.Add(emp);
+            UnitsDir.ItemsSource = units;
+
+            var query = from emp in context.Employees
+                        join unit in context.Units on emp.Id_Unit equals unit.Id_Unit
+                        select new
+                        {
+                            ID = emp.Id,
+                            EmployeeName = emp.FullName,
+                            UnitName = unit.Unit,
+                            Position = emp.Position,
+                            PhoneNumber = emp.PhoneNumber
+                        };
+
+            EmloyeesAndUnitsDir.ItemsSource = query.ToList();
+        }
+
+        //Where
+        private void FindByUnitName(object sender, RoutedEventArgs e)
+        {
+            var query = from emp in context.Employees
+                        join unit in context.Units on emp.Id_Unit equals unit.Id_Unit
+                        where unit.Unit == UnitBlock.Text
+                        select new
+                        {
+                            ID = emp.Id,
+                            EmployeeName = emp.FullName,
+                            UnitName = unit.Unit,
+                            Position = emp.Position,
+                            PhoneNumber = emp.PhoneNumber
+                        };
+
+            gridByUnitName.ItemsSource = query.ToList();
+
+            int employeeCount = query.Count();
+            UnitCountBlock.Text = employeeCount.ToString();
+        }
+
+        //
+        private void FindByPosition(object sender, RoutedEventArgs e)
+        {
+            var query = from emp in context.Employees
+                        join unit in context.Units on emp.Id_Unit equals unit.Id_Unit
+                        where emp.Position == PositionBlock.Text
+                        select new
+                        {
+                            ID = emp.Id,
+                            EmployeeName = emp.FullName,
+                            UnitName = unit.Unit,
+                            Position = emp.Position,
+                            PhoneNumber = emp.PhoneNumber
+                        };
+
+            gridByPosition.ItemsSource = query.ToList();
+
+            int employeeCount = query.Count();
+            PositionCountBlock.Text = employeeCount.ToString();
+        }
+
     }
 }
