c17df837a67b6829ac79144dc5c80b9431f09222
DarinaSmolianchuk <dsmolianchuk@gmail.com>
2024-05-16 20:56:58 +0300

    Added business logic

---
+++ Lab7/WpfApplProject/WpfApplProject/Commands/DataCommands.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+namespace WpfApplProject.Commands
+{
+    class DataCommands
+    {
+        public static RoutedCommand Undo { get; set; }
+        public static RoutedCommand New { get; set; }
+        public static RoutedCommand Replace { get; set; }
+        public static RoutedCommand Save { get; set; }
+        public static RoutedCommand Find { get; set; }
+        public static RoutedCommand Delete { get; set; }
+
+        static DataCommands()
+        {
+            //Колекція об'єктів
+            InputGestureCollection inputs = new InputGestureCollection();
+
+            //Команди
+            inputs.Add(new KeyGesture(Key.A, ModifierKeys.Control, "Ctrl+Z"));
+            Undo = new RoutedCommand("Undo", typeof(DataCommands), inputs);
+
+            inputs.Add(new KeyGesture(Key.N, ModifierKeys.Control, "Ctrl+N"));
+            New = new RoutedCommand("New", typeof(DataCommands), inputs);
+
+            inputs.Add(new KeyGesture(Key.R, ModifierKeys.Control, "Ctrl+R"));
+            Replace = new RoutedCommand("Replace", typeof(DataCommands), inputs);
+
+            inputs.Add(new KeyGesture(Key.S, ModifierKeys.Control, "Ctrl+S"));
+            Save = new RoutedCommand("Save", typeof(DataCommands), inputs);
+
+            inputs.Add(new KeyGesture(Key.D, ModifierKeys.Control, "Ctrl+F"));
+            Find = new RoutedCommand("Find", typeof(DataCommands), inputs);
+
+            inputs.Add(new KeyGesture(Key.D, ModifierKeys.Control, "Ctrl+D"));
+            Delete = new RoutedCommand("Delete", typeof(DataCommands), inputs);
+        }
+
+    }
+}
--- Lab7/WpfApplProject/WpfApplProject/PageTable1.xaml.cs
+++ Lab7/WpfApplProject/WpfApplProject/PageTable1.xaml.cs
@@ -1,28 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApplProject
 {
-    /// <summary>
-    /// Логика взаимодействия для PageTable1.xaml
-    /// </summary>
     public partial class PageTable1 : Page
     {
         public PageTable1()
         {
             InitializeComponent();
         }
+
+        private bool isDirty = true;
+
+        //Скасування
+        public void UndoCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void UndoCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Скасування");
+            isDirty = true;
+        }
+
+        //Створення
+        public void NewCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void NewCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Створення");
+            isDirty = true;
+        }
+
+        //Редагування
+        public void ReplaceCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void ReplaceCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Редагування");
+            isDirty = true;
+        }
+
+        //Збереження
+        public void SaveCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Збереження");
+            isDirty = true;
+        }
+
+        //Пошук
+        public void FindCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void FindCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Пошук");
+            isDirty = true;
+        }
+
+        //Видалення
+        public void DeleteCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void DeleteCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Видалення");
+            isDirty = true;
+        }
+
     }
 }
